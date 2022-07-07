6b27d1cae79b5419728b5e3193bb1f3b71cb4221
Gotmished <5002@holbertonschool.com>
2022-07-06 17:45:00 -0700

    Added test cases for printing chars

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 
 int main(void)
 {
+	printf("Put a char here: =>%c", 'c');
 	_printf("Put a char here: =>%c", 'c');
+	printf("Unknown: [%r]");
+	_printf("Unknown: [%r]");
+	printf("Unknown: [%%]");
+	_printf("Unknown: [%%]");
 	return (0);
 }
