21a3052c124e3a0f9ff53c3f8d93c590d94e874d
Gotmished <5002@holbertonschool.com>
2022-07-06 18:50:55 -0700

    moved main.c inro main folder

---
+++ main/main.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Put a char here: =>%c", 'c');
+	_printf("Put a char here: =>%c", 'c');
+	printf("Unknown: [%r]");
+	_printf("Unknown: [%r]");
+	printf("Unknown: [%%]");
+	_printf("Unknown: [%%]");
+	return (0);
+}
