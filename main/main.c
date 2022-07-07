02fefbe22ff5426b7c0998f17127ea1e62fe72d8
Luke Hackett <luke-hackett@outlook.com>
2022-07-06 19:01:16 -0700

    commented out %r due to compilation err

--- main/main.c
+++ main/main.c
@@ -1,13 +1,13 @@
 #include <stdarg.h>
-#include "main.h"
+#include "../main.h"
 #include <stdio.h>
 
 int main(void)
 {
 	printf("Put a char here: =>%c", 'c');
 	_printf("Put a char here: =>%c", 'c');
-	printf("Unknown: [%r]");
-	_printf("Unknown: [%r]");
+	/* printf("Unknown: [%r]"); */
+	/* _printf("Unknown: [%r]"); */
 	printf("Unknown: [%%]");
 	_printf("Unknown: [%%]");
 	return (0);
