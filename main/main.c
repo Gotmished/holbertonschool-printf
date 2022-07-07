ecc4f166c03b02abe411cfdedec55847b25a7da5
Luke Hackett <luke-hackett@outlook.com>
2022-07-06 19:06:10 -0700

    Removed %r

--- main/main.c
+++ main/main.c
@@ -6,8 +6,6 @@ int main(void)
 {
 	printf("Put a char here: =>%c", 'c');
 	_printf("Put a char here: =>%c", 'c');
-	/* printf("Unknown: [%r]"); */
-	/* _printf("Unknown: [%r]"); */
 	printf("Unknown: [%%]");
 	_printf("Unknown: [%%]");
 	return (0);
