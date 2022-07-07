85eb1fff93260aca390f46ef156d06680e8f8dbf
Luke Hackett <luke-hackett@outlook.com>
2022-07-06 23:26:30 -0700

    changed test

--- main/main.c
+++ main/main.c
@@ -1,12 +1,24 @@
-#include <stdarg.h>
-#include "../main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "../main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
 int main(void)
 {
-	printf("stdio: Put a char here: =>%c\n", 'e');
-	_printf("Ours:Put a char here: =>%c\n", 'e');
-	printf("Unknown: [%%]");
-	_printf("Unknown: [%%]");
+	int len, len2;
+
+	len = _printf("%s", "This sentence is retrieved from va_args!\n");
+	len2 = printf("%s", "This sentence is retrieved from va_args!\n");
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	return (0);
 }
