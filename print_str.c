e51e7cf84c26ba1f60ddaf37d266c5dd14315f43
Luke Hackett <luke-hackett@outlook.com>
2022-07-07 19:13:51 -0700

    Made print str own file

---
+++ print_str.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_s - print a string passed through arg
+ * @list: list pointer string
+ * Return: int, count of string length
+ */
+
+int print_s(va_list list)
+{
+	int count;
+	char *a;
+
+	a = va_arg(list, char *);
+	if (a == NULL)
+	{
+		return (0);
+	}
+/*	printf("string to print: %s\n", va_arg(list, char *)); */
+
+	count = 0;
+	while (a[count] != '\0')
+	{
+		_putchar(a[count]);
+		count = count + 1;
+	}
+
+	return (count);
+}
