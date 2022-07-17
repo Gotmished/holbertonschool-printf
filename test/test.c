bbddd894247a203f9c6aad7790cd2891273c44bf
Luke Hackett <luke-hackett@outlook.com>
2022-07-16 20:22:52 -0700

    test file for binary

--- test/test.c
+++ test/test.c
@@ -1,26 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "../main.h"
 
 /**
  * main - Entry point
  *
- * Return: 0 on success, error code otherwise
+ * Return: Always 0
  */
 int main(void)
 {
-	int len, len2;
-
-	len = _printf("%");
-	len2 = printf("%");
-	fflush(stdout);
-	printf("len: %d\n", len);
-	printf("len2: %d\n", len2);
-	if (len != len2)
-	{
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
+	_printf("%b\n", -1024);
+	_printf("%b\n", 1024);
 	return (0);
 }
